40dc10cab19ce4b56e521c68aebc615811e40990
yyyns <2024977971@qq.com>
2020-11-24 17:36:27 +0800

    bla

---
+++ Assets/Scenes/Base/Base.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Base : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Skip(int destination)
+    {
+        SceneManager.LoadScene(destination);
+    }
+
+    
+}
