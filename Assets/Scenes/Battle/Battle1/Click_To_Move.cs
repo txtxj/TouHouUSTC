39c9aeab9e4e201b5f712b5d9152e0976e1a3879
txtxj <1418373194@qq.com>
2020-11-25 17:12:37 +0800

    添加了移动功能

---
+++ Assets/Scenes/Battle/Battle1/Click_To_Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click_To_Move : MonoBehaviour
+{
+	public GameObject Selected_Character;
+	Vector3[] Position=new Vector3[2];
+	int Corner=0;
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+	// Only called in function Move_To_Destination()
+	void Move()
+	{
+		if (Corner<2)
+		{
+			iTween.MoveTo(Selected_Character,iTween.Hash("position",Position[Corner],"easeType","linear","speed",12f,"oncomplete","Move","oncompletetarget",gameObject));
+			Corner++;
+		}
+	}
+	void Move_To_Destination()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray=GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast(ray,out hit);
+			// 先横着走再竖着走
+			Position[0]=new Vector3(hit.point.x,Selected_Character.transform.position.y,-3);
+			Position[1]=new Vector3(hit.point.x,hit.point.y,-3);
+			Corner=0;
+			Move();
+		}
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		Move_To_Destination();
+	}
+}
--- Assets/Scenes/Battle/BattleLayout.cs
+++ Assets/Scenes/Battle/BattleLayout.cs
@@ -1,45 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public struct WindowCB //窗体控制的真值结构体
-{
-    public bool cvPause,cvInfoMenu,cvScoreMenu;
-}
-static public class State{
-    public static WindowCB isFadingOut;
-    public static WindowCB isFadingIn;
-}
-public class BattleLayout : MonoBehaviour
-{
-    public GameObject cvPause;
-    int count;
-    int windowFadeInAndOutTime;
-    float EaseOut(int frame)
-    {
-        if (frame < windowFadeInAndOutTime / 3.0)
-        {
-            return (-2.0 / windowFadeInAndOutTime * frame + 1);
-        }
-        else
-        {
-            return ();
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        cvPause.gameObject.SetActive(false);
-        State.isFadingOut.cvPause = true;
-        count = 0;
-    }
+// public struct WindowCB //窗体控制的真值结构体
+// {
+//     public bool cvPause,cvInfoMenu,cvScoreMenu;
+// }
+// static public class State{
+//     public static WindowCB isFadingOut;
+//     public static WindowCB isFadingIn;
+// }
+// public class BattleLayout : MonoBehaviour
+// {
+//     public GameObject cvPause;
+//     int count;
+//     int windowFadeInAndOutTime;
+//     float EaseOut(int frame)
+//     {
+//         if (frame < windowFadeInAndOutTime / 3.0)
+//         {
+//             return (-2.0 / windowFadeInAndOutTime * frame + 1);
+//         }
+//         else
+//         {
+//             return 0.0;
+//         }
+//     }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         cvPause.gameObject.SetActive(false);
+//         State.isFadingOut.cvPause = true;
+//         count = 0;
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (State.isFadingOut.cvPause)
-        {
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (State.isFadingOut.cvPause)
+//         {
 
-        }
-    }
-}
+//         }
+//     }
+// }
